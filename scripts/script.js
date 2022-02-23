ab5f6e025f33cc722b15efaa3c1362c0996d37b6
Ilya Pavlov <ilya18052000@gmail.com>
2022-02-24 00:33:42 +0300

    added smth

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-console.log('ama live!');
+
+jQuery(document).ready(function($) {
+
+    console.log('ama live!');
+
+    var sliderTwo = $('#carouselExampleIndicators2').carousel({
+        /* your options for slider #2 */
+      })
+      
+      $(".slider-pager li").click(function (e) {
+          var index = $(this).data('slide');
+          sliderTwo.carousel(index);
+          e.preventDefault();
+      });
+
+});
